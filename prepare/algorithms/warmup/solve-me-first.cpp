d21ceba9c29ca4a5a78b20559a29b2302927631b
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2025-01-29 12:30:41 -0600

    solve-me-first.cpp solution

---
+++ prepare/algorithms/warmup/solve-me-first.cpp
@@ -0,0 +1,39 @@
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+a=7
+b=3
+Return 10.
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
